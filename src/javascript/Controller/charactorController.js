aaeefdb2f5959b9f478cf434bd6a60cb9903ebf1
kazuman <noblesse-oblige_23@i.softbank.jp>
2015-04-27 12:42:02 +0900

    ok

---
+++ src/javascript/Controller/charactorController.js
@@ -0,0 +1,13 @@
+// コントローラの状態ふらぐがはいります
+var flag;
+
+// キャラクターのリスト
+var charactorList;
+
+//現在画面にデテいるキャラクター
+var currentCharactor;
+
+// キャラクターのリストをしゅとくします
+function getCharactorList (argument) {
+  // body...
+}
---
+++ src/javascript/Controller/mainViewController.js
@@ -0,0 +1,41 @@
+
+
+// これから作ります
+var test;
+
+// ログは汎用的に使わないで、できるだけ専用のログの変数をつくる。(汎用的にするのはめんどい)
+var log;
+
+// メッセのログ
+var logMessage;
+
+// キャラ名のログ
+var logCharactorName;
+
+// 状態のログ
+var logStatus;
+
+// フラグのろぐ
+var logFlag;
+
+// データベースのkeyはスネークケースで書く
+var char_id;
+
+var dere_flag;
+var tsun_flag;
+var tsun_dere_flag;
+var hoge_flag;
+var char_status;
+var hoge_status;
+
+function logMessage (argument) {
+  // body...
+}
+
+function getLog (argument) {
+  // body...
+}
+
+function getCharactorController (argument) {
+  // body...
+}
