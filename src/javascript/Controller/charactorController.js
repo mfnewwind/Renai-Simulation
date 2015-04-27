fffec249d6069acd492c463f8944ffa62011786d
kazuman <noblesse-oblige_23@i.softbank.jp>
2015-04-27 12:50:23 +0900

    create object

--- src/javascript/Controller/charactorController.js
+++ src/javascript/Controller/charactorController.js
@@ -1,6 +1,16 @@
 // コントローラの状態ふらぐがはいります
 var flag;
 
+// 意味わかんないッ
+var maki;
+// 各キャラクターを変数として持つのダサすぎる。。。
+var ui;
+var hanayo;
+var kotori;
+var niko;
+var rin;
+var honoka;
+
 // キャラクターのリスト
 var charactorList;
 
