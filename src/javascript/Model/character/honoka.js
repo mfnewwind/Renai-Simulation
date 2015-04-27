bfe195addb5b6332cef259774637b2fa795bd3aa
kazuman <noblesse-oblige_23@i.softbank.jp>
2015-04-27 12:36:14 +0900

    create new View, charactor

---
+++ src/javascript/Model/character/honoka.js
@@ -0,0 +1,115 @@
+// デレてるか、怒っているかといった感情に関するどれかのフラグが入ります
+var flag;
+// 状態
+var status;
+// 強さ
+var strength;
+
+// testStatus
+var hoge;
+// template
+var temp;
+
+// キャラクター特有のフラグ
+var characterOfFlag;
+
+// デレてるかの状態のときにたつ
+var isDereteru;
+// ツンかの状態のときたつ
+var isDereteru;
+
+
+// テスト用関数
+function hoge() {
+
+}
+
+//　デレさせます
+function dereru() {
+  // デレるフラグ
+  var flag;
+  // デレてるかの状態を入れておくtemp
+  var temp; 
+
+  // デレてるのステータスをテストでいれておく
+  var hoge;
+}
+
+// ツンツンになります
+function tsun() {
+  // ツンツン状態かのフラグ
+  var flag;
+
+  // デレてるかの状態を入れておくtemp
+  var temp; 
+
+  // デレてるのステータスをテストでいれておく
+  var hoge;
+}
+
+// ツンデレになります
+function tsunDere() {
+  // ツンデレ状態かのフラグ
+  var flag;
+
+  // ツンデレの状態を入れておくtemp
+  var temp; 
+
+  // ツンデレてるのステータスをテストでいれておく
+  var hoge;
+}
+
+
+function getMessage() {
+
+}
+
+// メッセージを読み込みます
+function getMessage() {
+  var message;
+  var tempMessage;
+}
+
+
+// 引数に指定したactionをおこします
+function action() {
+  // テスト用のアクションが入ってます
+  var hoge;
+}
+
+// プロパティからステータスをとります
+function getStatus() {
+
+}
+
+// データベースからステータスをとります
+function fetchStatus() {
+  // データベースからとったステータス
+  var satatus;
+
+}
+
+// プロパティにステータスを入れます
+function getStatus() {
+  // プロパティに入れるステータス
+  var satatus;
+}
+
+// データベースにステータスを入れます
+function storeStatus() {
+  // データベースに入れるステータス
+  var satatus;
+}
+
+// あとで使うかもしれないステータスを入れます
+function registerStatus() {
+  // あとで使うかもしれないステータス
+  var satatus;
+}
+
+// 一応生やしておく
+var fuga;
+// テストでセットするために生やしてます
+function setFuga() {
+
+}
---
+++ src/javascript/View/gameView.js
@@ -0,0 +1,11 @@
+// 画面の状態ふらぐがはいります
+var flag;
+
+// 状態
+var status;
+
+// mainGamenとわけたいけれど名前が思いつかないからとりあえずGamen
+var mainGamen;
+
+// 画面のオブジェクト
+var mainScreen;
